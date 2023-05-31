5eabb49b1da680562e01b6329d2d0cb9b79e34cf
weare4saken <kondrashka91@gmail.com>
2023-05-31 23:29:53 +0700

    Initial commit

---
+++ src/main/java/com/weare4saken/pcstore/PcStoreApplication.java
@@ -0,0 +1,13 @@
+package com.weare4saken.pcstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weare4saken/pcstore/PcStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weare4saken.pcstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
